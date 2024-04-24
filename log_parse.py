25d3d44ba49b556c2d8f3b0ffed21ece0851edba
Spencer Morgan <spencermorgan@spencers-air.mynetworksettings.com>
2024-04-23 23:15:54 -0400

    added comments to each section or the error handling

--- log_parse.py
+++ log_parse.py
@@ -5,12 +5,15 @@ def ingest_csv(csv_file):
     try:
         df = pd.read_csv(csv_file)
         return df
+     # Present error message if .csv does not exist
     except FileNotFoundError:
         print(f"Error: {csv_file} not found.")
         return None
+    # Present error if .csv has no data
     except pd.errors.EmptyDataError:
         print(f"Error: {csv_file} is empty.")
         return None
+    # Present error if .csv fails to parse provided data
     except pd.errors.ParserError:
         print(f"Error: Unable to parse {csv_file}.")
         return None
