521b27d413b69b6c14c2baef2914390120a420b7
spencermrgn <52977600+spencermrgn@users.noreply.github.com>
2024-04-25 11:05:40 -0400

    Update log_parse.py comments

--- log_parse.py
+++ log_parse.py
@@ -2,25 +2,25 @@ import pandas as pd
 import sys
 
 def ingest_csv(csv_file):
-    # Read the CSV file into a pandas DataFrame
+    #Read the CSV file into a pandas DataFrame
     try:
         df = pd.read_csv(csv_file)
         return df
-     # Present error message if .csv does not exist
+    #Present error message if .csv does not exist
     except FileNotFoundError:
         print(f"Error: {csv_file} not found.")
         return None
-    # Present error if .csv has no data
+    #Present error if .csv has no data
     except pd.errors.EmptyDataError:
         print(f"Error: {csv_file} is empty.")
         return None
-    # Present error if .csv fails to parse provided data
+    #Present error if .csv fails to parse provided data
     except pd.errors.ParserError:
         print(f"Error: Unable to parse {csv_file}.")
         return None
     
 def filter_event_ids(df):
-    # filter dataframe on specified event ids.....modify ids as needed
+    #filter dataframe on specified event ids.....modify ids as needed
     filtered_df = df[df['EventID'].isin([4742, 5136])]
     return filtered_df
 
@@ -35,7 +35,7 @@ def main():
     csv_file = sys.argv[1]
     df = ingest_csv(csv_file)
     if df is not None:
-        print("CSV file ingested successfully.")
+        print(".csv file ingested successfully.")
         # Filter events with EventID 4742 or 5136
         filtered_df = filter_events(df)
         if not filtered_df.empty:
